462d7cc37cfea421c534d44812d8b5b921ac2315
cellerligia-23 <cellerligia23@gmail.com>
2024-03-21 09:21:55 -0300

    Ajustes do CSS e alteração no nome dos arquivos js

--- src/js/api-pokemon.js
+++ src/js/api-pokemon.js
@@ -1,7 +1,40 @@
-class Pokemon {
-  name;
-  types = []
-  type;
-  order;
-  imgSrc;
-}
+/*
+API Pokémons
+URL: https://pokeapi.co/api/v2/pokemon/
+*/
+
+const pokeApi = {
+  convertPokemonModel: (pokemonsDetail) => {
+    const pokemons = new Pokemon();
+
+    pokemons.order = pokemonsDetail.order;
+    pokemons.name = pokemonsDetail.name;
+    pokemons.types = pokemonsDetail.types.map(
+      (pokemonTypes) => pokemonTypes.type.name
+    );
+    pokemons.type = pokemons.types[0];
+    pokemons.imgSrc = pokemonsDetail.sprites.other.dream_world.front_default;
+
+    // console.log(pokemons);
+    return pokemons;
+  },
+  getPokemonsDetail: (pokemon) => {
+    return fetch(pokemon.url)
+      .then((response) => response.json())
+      .then(pokeApi.convertPokemonModel)
+      .catch((error) => console.error(error));
+    // .finally(() => console.log('Requisição getPokemonsDetail executa!'))
+  },
+  getPokemons: (offset = 0, limit = 7) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+      .then((response) => response.json())
+      .then((jsonBody) => jsonBody.results)
+      .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+      .then((detailRequests) => Promise.all(detailRequests))
+      .then((pokemonsDetail) => pokemonsDetail)
+      .catch((error) => console.error(error));
+    // .finally(() => console.log('Requisição getPokemos executa!'))
+  },
+};
